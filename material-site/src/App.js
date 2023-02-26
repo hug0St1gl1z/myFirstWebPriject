668ba67bfa168650b2dffd051ba2afbcffece8e2
hug0St1gl1z <ivan.filin@gmail.com>
2023-02-26 13:36:37 -0500

    1

---
+++ material-site/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained"> Кнопка 1 </Button>
+      <p/>
+      <Button variant="contained" color="primary"> Кнопка 2 </Button>
+
+    </div>
+  );
+}
+
+export default App;
